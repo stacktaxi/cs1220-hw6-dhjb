252405920fbeba735e68b77caf961200e442c502
stacktaxi <stacktaxi@openmailbox.org>
2015-12-03 18:41:27 -0500

    fixed buggy nand

--- source/Nand.cpp
+++ source/Nand.cpp
@@ -1,13 +1,13 @@
 #include "Nand.h"
 
 TriState Nand::recompute() {
-	if(in[0]->getValue() == 1 || in[1]->getValue() == 1) {
-		return 0;
+	if(in[0]->getValue() == 0 || in[1]->getValue() == 0) {
+		return 1;
 	}
 	else if(in[0]->getValue() == X || in[1]->getValue() == X) {
 		return X;
 	}
-	else { // Both inputs are zero.
-		return 1;
+	else { // Both inputs are 1. 
+		return 0;
 	}
 }
